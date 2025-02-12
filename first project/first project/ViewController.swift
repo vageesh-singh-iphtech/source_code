9429b1342cdf3d78a0367a0bc35c823513ddf8c4
vageesh-singh-iphtech <vageesh.singh@iphtechnologies.com>
2025-02-12 18:28:28 +0530

    totorial 1 pages

---
+++ first project/first project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  first project
+//
+//  Created by iPHTech 35 on 12/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
